dac8eb572efc1ffdcf07100d7e46f20daec246ba
Mohammed Saleh <mssaleh@users.noreply.github.com>
2018-10-01 02:49:47 +0400

    Update st-mqtt-bifrost-device.groovy

--- devicetypes/mssaleh/st-mqtt-bifrost-device.src/st-mqtt-bifrost-device.groovy
+++ devicetypes/mssaleh/st-mqtt-bifrost-device.src/st-mqtt-bifrost-device.groovy
@@ -4,7 +4,7 @@ import groovy.json.JsonSlurper
 import groovy.json.JsonOutput
 
 metadata {
-    definition (name: "MQTT Bridge", namespace: "mssaleh", author: "Mohammed Saleh") {
+    definition (name: "ST-MQTT Bifrost Device", namespace: "mssaleh", author: "Mohammed Saleh") {
         capability "Notification"
     }
 
