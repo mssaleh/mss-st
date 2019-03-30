37a7cb47cb0ad5842310eff571e37c344ef43b5b
Mohammed Saleh <mssaleh@users.noreply.github.com>
2019-03-30 10:28:14 +0400

    merged with johndoyle code

--- devicetypes/mssaleh/xiaomi-aqara-wired-single-switch.src/xiaomi-aqara-wired-single-switch.groovy
+++ devicetypes/mssaleh/xiaomi-aqara-wired-single-switch.src/xiaomi-aqara-wired-single-switch.groovy
@@ -1,18 +1,16 @@
+// source https://raw.githubusercontent.com/johndoyle/SmartThingsPersonal/master/devicetypes/Xiaomi/xiaomi-mains-single-switch-no-neutral.groovy
+
 metadata {
     definition (name: "Xiaomi Aqara Wired Single Switch", namespace: "mssaleh", author: "Mohammed Saleh", vid: "generic-switch") {
-        capability "Actuator"
-        capability "Configuration"
-        capability "Refresh"
-        capability "Switch"
-        capability "Temperature Measurement"
+    capability "Actuator"
+    capability "Configuration"
+    capability "Refresh"
+    capability "Momentary"
+    capability "Switch"
+    capability "Temperature Measurement"
 
-        command "on2"
-        command "off2"
-
-        attribute "switch2","ENUM", ["on","off"]
-
-        attribute "lastCheckin", "string"
-    }
+    fingerprint profileId: "0104", inClusters: "0000, 0002, 0006"
+	}
 
     // simulator metadata
     simulator {
@@ -24,57 +22,75 @@ metadata {
         reply "zcl on-off on": "on/off: 1"
         reply "zcl on-off off": "on/off: 0"
     }
+	
+    preferences {
+        input "tempOffset", "number", title: "Temperature Offset", description: "Adjust temperature in degrees",
+              range: "*..*", displayDuringSetup: false
+		input title: "", description: "Use to correct any temperature variations by selecting an offset.", displayDuringSetup: false, type: "paragraph", element: "paragraph"
+		input "debugOutput", "boolean", 
+			title: "Enable debug logging?",
+            description: "Use to display messages in Live Logging.",
+			defaultValue: false,
+			displayDuringSetup: true
+	}
 
+ // UI tile definitions
     tiles(scale: 2) {
-        multiAttributeTile(name:"switch", type: "lighting", width: 6, height: 4, canChangeIcon: true){
+        multiAttributeTile(name:"rich-control", type: "switch", canChangeIcon: false){
             tileAttribute ("device.switch", key: "PRIMARY_CONTROL") {
-                attributeState "on", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
-                attributeState "off", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
-                attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.switches.light.on", backgroundColor:"#00a0dc", nextState:"turningOff"
-                attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.switches.light.off", backgroundColor:"#ffffff", nextState:"turningOn"
-            }
-
-
-
-           	tileAttribute("device.lastCheckin", key: "SECONDARY_CONTROL") {
-    			attributeState("default", label:'Last Update: ${currentValue}',icon: "st.Health & Wellness.health9")
-		   	}
+                 attributeState "on", label:'${name}', action:"switch.off", icon:"st.Home.home30", backgroundColor:"#00A0DC", nextState:"turningOff"
+                 attributeState "off", label:'${name}', action:"switch.on", icon:"st.Home.home30", backgroundColor:"#ffffff", nextState:"turningOn"
+                 attributeState "turningOn", label:'${name}', action:"switch.off", icon:"st.Home.home30", backgroundColor:"#00A0DC", nextState:"turningOff"
+                 attributeState "turningOff", label:'${name}', action:"switch.on", icon:"st.Home.home30", backgroundColor:"#ffffff", nextState:"turningOn"
+                 attributeState "offline", label:'${name}', icon:"st.Home.home30", backgroundColor:"#cccccc"
+ 			}
         }
 
-    //     standardTile("switch2", "device.switch2", width: 2, height: 2, canChangeIcon: true) {
-		// 	state "off", label: '${name}', action: "on2", icon: "st.switches.switch.off", backgroundColor: "#ffffff"
-		// 	state "on", label: '${name}', action: "off2", icon: "st.switches.switch.on", backgroundColor: "#79b821"
-		// }
+        standardTile("switch", "device.switch", width: 2, height: 2, canChangeIcon: false) {
+            state "on", label:'${name}', action:"switch.off", icon:"st.Home.home30", backgroundColor:"#00A0DC", nextState:"turningOff"
+            state "off", label:'${name}', action:"switch.on", icon:"st.Home.home30", backgroundColor:"#ffffff", nextState:"turningOn"
+            state "turningOn", label:'${name}', action:"switch.off", icon:"st.Home.home30", backgroundColor:"#00A0DC", nextState:"turningOff"
+            state "turningOff", label:'${name}', action:"switch.on", icon:"st.Home.home30", backgroundColor:"#ffffff", nextState:"turningOn"
+            state "offline", label:'${name}', icon:"st.Home.home30", backgroundColor:"#cccccc"
+        }
+
+        standardTile("refresh", "device.switch", inactiveLabel: false, height: 2, width: 2, decoration: "flat") {
+            state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
+        }
 
         valueTile("temperature", "device.temperature", width: 2, height: 2) {
-			state("temperature", label:'${currentValue}°',
+			state("temperature", label:'${currentValue}°', unit:"C",
 				backgroundColors:[
-					[value: 31, color: "#153591"],
+					[value: 0, color: "#153591"],
+					[value: 5, color: "#1e9cbb"],
+					[value: 10, color: "#90d2a7"],
+					[value: 15, color: "#44b621"],
+					[value: 20, color: "#f1d801"],
+					[value: 25, color: "#d04e00"],
+					[value: 30, color: "#bc2323"],
 					[value: 44, color: "#1e9cbb"],
 					[value: 59, color: "#90d2a7"],
 					[value: 74, color: "#44b621"],
 					[value: 84, color: "#f1d801"],
 					[value: 95, color: "#d04e00"],
-					[value: 96, color: "#bc2323"]
+					[value: 96, color: "#bc2323"]  
 				]
 			)
 		}
         standardTile("refresh", "device.refresh", inactiveLabel: false, decoration: "flat", width: 2, height: 2) {
             state "default", label:"", action:"refresh.refresh", icon:"st.secondary.refresh"
         }
-        main (["switch", "temperature"])
-        details(["switch", "temperature", "refresh"])
+        main(["switch"])
+        details(["rich-control", "temperature", "refresh"])
     }
 }
 
 // Parse incoming device messages to generate events
 def parse(String description) {
-   log.debug "Parsing '${description}'"
-   def value = zigbee.parse(description)?.text
-   log.debug "Parse: $value"
-   Map map = [:]
-
-   if (description?.startsWith('catchall:')) {
+	def value = zigbee.parse(description)?.text
+	Map map = [:]
+   
+	if (description?.startsWith('catchall:')) {
 		map = parseCatchAllMessage(description)
 	}
 	else if (description?.startsWith('read attr -')) {
@@ -82,16 +98,8 @@ def parse(String description) {
 	}
     else if (description?.startsWith('on/off: ')){
     	def resultMap = zigbee.getKnownDescription(description)
-   		log.debug "${resultMap}"
-
-        map = parseCustomMessage(description)
+        map = parseCustomMessage(description) 
     }
-
-	log.debug "Parse returned $map"
-    //  send event for heartbeat
-    def now = new Date()
-    sendEvent(name: "lastCheckin", value: now)
-
 	def results = map ? createEvent(map) : null
 	return results;
 }
@@ -99,8 +107,7 @@ def parse(String description) {
 private Map parseCatchAllMessage(String description) {
 	Map resultMap = [:]
 	def cluster = zigbee.parse(description)
-	log.debug cluster
-
+    
     if (cluster.clusterId == 0x0006 && cluster.command == 0x01){
     	def onoff = cluster.data[-1]
         if (onoff == 1)
@@ -108,77 +115,100 @@ private Map parseCatchAllMessage(String description) {
         else if (onoff == 0)
             resultMap = createEvent(name: "switch", value: "off")
     }
-
+    else if (cluster.clusterId == 0x0000 && cluster.command == 0x0a){
+    	if (state.debug) log.info "Periodic Update"
+    }
 	return resultMap
 }
-
 private Map parseReportAttributeMessage(String description) {
 	Map descMap = (description - "read attr - ").split(",").inject([:]) { map, param ->
 		def nameAndValue = param.split(":")
 		map += [(nameAndValue[0].trim()):nameAndValue[1].trim()]
 	}
-	//log.debug "Desc Map: $descMap"
 
 	Map resultMap = [:]
 
-	if (descMap.cluster == "0001" && descMap.attrId == "0020") {
-		resultMap = getBatteryResult(convertHexToInt(descMap.value / 2))
- 	}
     if (descMap.cluster == "0002" && descMap.attrId == "0000") {
-		resultMap = createEvent(name: "temperature", value: zigbee.parseHATemperatureValue("temperature: " + (convertHexToInt(descMap.value) / 2), "temperature: ", getTemperatureScale()), unit: getTemperatureScale())
-		log.debug "Temperature Hex convert to ${resultMap.value}%"
-    }
+    	def temp = convertHexToInt(descMap.value)
+        if (tempOffset) {
+			temp = (int) temp + (int) tempOffset
+		}
+    	if (state.debug) log.info "Reported Temp of " + temp + "°" + getTemperatureScale()
+		resultMap = createEvent(name: "temperature", value: zigbee.parseHATemperatureValue("temperature: " + temp, "temperature: ", getTemperatureScale()), unit: getTemperatureScale())
+	}
     else if (descMap.cluster == "0008" && descMap.attrId == "0000") {
+    	if (state.debug) log.info "Reported Switch Off"
     	resultMap = createEvent(name: "switch", value: "off")
-    }
+    } 
 	return resultMap
+}
+private Map parseCustomMessage(String description) {
+	def result
+	if (!isDuplicateCommand(state.lastUpdated, 2000)) {
+        state.lastUpdated = new Date().time
+        if (description?.startsWith('on/off: ')) {
+            if (description == 'on/off: 0')
+                result = createEvent(name: "switch", value: "off")
+            else if (description == 'on/off: 1')
+                result = createEvent(name: "switch", value: "on")
+        }
+	}
+    return result
+}
+private isDuplicateCommand(lastExecuted, allowedMil) {
+	!lastExecuted ? false : (lastExecuted + allowedMil > new Date().time)
 }
 
 def off() {
-    log.debug "off()"
-	sendEvent(name: "switch", value: "off")
-	"st cmd 0x${device.deviceNetworkId} 2 6 0 {}"
+	if (device.endpointId == null) device.endpointId = 2
+   	if (state.debug) log.info "Off()"
+    zigbee.off()
 }
 
 def on() {
-   log.debug "on()"
-	sendEvent(name: "switch", value: "on")
-	"st cmd 0x${device.deviceNetworkId} 2 6 1 {}"
+	if (device.endpointId == null) device.endpointId = 2
+   	if (state.debug) log.info "On()"
+    zigbee.on()
 }
 
-def off2() {
-    log.debug "off2()"
-	sendEvent(name: "switch2", value: "off")
-	"st cmd 0x${device.deviceNetworkId} 3 6 0 {}"
+/**
+ * PING is used by Device-Watch in attempt to reach the Device
+ * */
+def ping() {
+   	if (state.debug) log.info "ping()"
+	if (device.endpointId == null) device.endpointId = 2
+    return zigbee.readAttribute(0x0006, 0x0000) +
+        zigbee.readAttribute(0x0008, 0x0000) +
+        zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 600, null) +
+        zigbee.configureReporting(0x0008, 0x0000, 0x20, 1, 3600, 0x01)
 }
 
-def on2() {
-   log.debug "on2()"
-	sendEvent(name: "switch2", value: "on")
-	"st cmd 0x${device.deviceNetworkId} 3 6 1 {}"
+def push() {
+   	if (state.debug) log.info "push()"
+	if (device.endpointId == null) device.endpointId = 2
+	sendEvent(name: "switch", value: "on", isStateChange: true, displayed: false)
+	sendEvent(name: "switch", value: "off", isStateChange: true, displayed: false)
+	sendEvent(name: "momentary", value: "pushed", isStateChange: true)
 }
 
 def refresh() {
-	log.debug "refreshing"
+	state.debug = ("true" == debugOutput)
+	if (state.debug) log.info "Refresh..."
     [
-        "st rattr 0x${device.deviceNetworkId} 1 6 0", "delay 500",
-        "st rattr 0x${device.deviceNetworkId} 1 6 0", "delay 250",
-        "st rattr 0x${device.deviceNetworkId} 1 2 0", "delay 250",
-        "st rattr 0x${device.deviceNetworkId} 1 1 0", "delay 250",
-        "st rattr 0x${device.deviceNetworkId} 1 0 0"
+        "st rattr 0x${device.deviceNetworkId} 0x01 0x0002 0x0000", "delay 250", // CurrentTemperature
+		"st rattr 0x${device.deviceNetworkId} 0x02 0x0006 0x0000", "delay 500", // On/Off
+		"st rattr 0x${device.deviceNetworkId} 0x02 0x0008 0x0000", "delay 250",  // On/Off
+		"st rattr 0x${device.deviceNetworkId} 0x02 0x0006 0x0000 0x10 0 600 null", "delay 500",
+		"st rattr 0x${device.deviceNetworkId} 0x02 0x0008 0x0000 0x20 1 3600 0x01"
     ]
-}
 
-private Map parseCustomMessage(String description) {
-	def result
-	if (description?.startsWith('on/off: ')) {
-    	if (description == 'on/off: 0')
-    		result = createEvent(name: "switch", value: "off")
-    	else if (description == 'on/off: 1')
-    		result = createEvent(name: "switch", value: "on")
-	}
-
-    return result
+def configure() {
+    if (state.debug) log.info "Configuring Reporting and Bindings."
+	if (device.endpointId == null) device.endpointId = 2
+    return zigbee.configureReporting(0x0006, 0x0000, 0x10, 0, 600, null) +
+        zigbee.configureReporting(0x0008, 0x0000, 0x20, 1, 3600, 0x01) +
+        zigbee.readAttribute(0x0006, 0x0000) +
+        zigbee.readAttribute(0x0008, 0x0000)
 }
 
 private Integer convertHexToInt(hex) {
